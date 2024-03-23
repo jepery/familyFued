bba15d83dda73f2a15458cf5e6587a300fbc423e
Jeffry Dela Cruz <jeperydcruz@gmail.com>
2024-03-24 06:28:16 +0800

    test first survey

---
+++ script.js
@@ -0,0 +1,47 @@
+surveybutton1.onclick = function(){
+
+    surveybutton1.style.display = "none";
+    survey1.style.display = "block"
+};
+
+surveybutton2.onclick = function(){
+
+    surveybutton2.style.display = "none";
+    survey2.style.display = "block"
+};
+
+surveybutton3.onclick = function(){
+
+    surveybutton3.style.display = "none";
+    survey3.style.display = "block"
+};
+
+surveybutton4.onclick = function(){
+
+    surveybutton4.style.display = "none";
+    survey4.style.display = "block"
+};
+
+surveybutton5.onclick = function(){
+
+    surveybutton5.style.display = "none";
+    survey5.style.display = "block"
+};
+
+surveybutton6.onclick = function(){
+
+    surveybutton6.style.display = "none";
+    survey6.style.display = "block"
+};
+
+surveybutton7.onclick = function(){
+
+    surveybutton7.style.display = "none";
+    survey7.style.display = "block"
+};
+
+surveybutton8.onclick = function(){
+
+    surveybutton8.style.display = "none";
+    survey8.style.display = "block"
+};
