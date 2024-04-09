73619264fb1d6c18a48819d633124bcc83f39954
Jeffry Dela Cruz <jeperydcruz@gmail.com>
2024-04-09 18:03:19 +0800

    add time

--- time.js
+++ time.js
@@ -1,25 +1,28 @@
 let timeSecond = 20;
-const timeH = document.querySelector("h1");
+    const timeH = document.querySelector("h1");
+    let countDownInterval;
 
-displayTime(timeSecond);
+    function startCountdown() {
+      clearInterval(countDownInterval); // Clear previous interval if any
+      displayTime(timeSecond);
+      countDownInterval = setInterval(() => {
+        timeSecond--;
+        displayTime(timeSecond);
+        if (timeSecond <= 0) {
+          endCount();
+          clearInterval(countDownInterval);
+        }
+      }, 1000);
+    }
 
-const countDown = setInterval(() => {
-  timeSecond--;
-  displayTime(timeSecond);
-  if (timeSecond == 0 || timeSecond < 1) {
-    endCount();
-    clearInterval(countDown);
-  }
-}, 1000);
+    function displayTime(second) {
+      const min = Math.floor(second / 60);
+      const sec = Math.floor(second % 60);
+      timeH.innerHTML = `
+        ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+      `;
+    }
 
-function displayTime(second) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  timeH.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
-}
-
-function endCount() {
-  timeH.innerHTML = "Times Up";
-}
+    function endCount() {
+      timeH.innerHTML = "Time's Up!";
+    }
